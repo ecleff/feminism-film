6ff53dee51fbd1f8e2ce192a0a1712b05c814be0
Emily Leff <ecleff@gmail.com>
2023-03-31 13:06:54 -0400

    grouped data with color

--- script.js
+++ script.js
@@ -21,57 +21,14 @@ const svg = d3
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
-//Read the data
-d3.csv("movies_1997_2013.csv").then(function (data) {
-  data.forEach(function (d) {
-    d["budget"] = +d["budget"];
-    d["domgross"] = +d["domgross"];
-    d["intgross"] = +d["intgross"];
-    
-    // console.log(d)
-  });
-ok = Object.keys(data[0])
-console.log(ok)
+  grouped = d3.csv("movies_grouped.csv").then(function (data) {
+    data.forEach(function (d) {
+      d["count_movies"] = +d["count_movies"];
+      d["avg_budget"] = +d["avg_budget"];
+      
+      console.log(d)
+    });
 
-// average budget by results + year
-avgBudget = d3.rollups(
-  data,
-  v => ({
-    count: v.length,
-    mean_budget: d3.mean(v, d => +d.budget), // get the average estimated project cost!
-  }),
-  d => d.year,
-  d => d.viz_results
-  
-)
-console.log(avgBudget)
-
-
-aB_data = avgBudget.map(d => {
-  let obj = {};
-  const [key, values] = d;
-  obj = { key: key, count: values[1], avg: values[2]};
-  return obj;
-  
-})
-
-console.log(aB_data)
-
-
-test = avgBudget.map(
-  // let obj = {};
-  // obj = {key: d.key, peryear:{
-  //   [d.values.count]: d.values.mean_budget
-  // }}
-  d => d.year
-)
-
-// console.log(test)
-
-
-
-  // Add X axis
-  // //   .style("fill", d => myColor(d.clean_test))
 
   const x = d3.scaleBand()
   .range([0, width])
@@ -109,25 +66,59 @@ const myColor = d3.scaleOrdinal()
     .range(d3.schemeSet1);
 
 // Add dots
+
 svg.append('g')
 .selectAll("dot")
-.data(aB_data)
+.data(data)
 .join("circle")
-  // .attr("class", function(d) { return "bubbles " + d.key })
-  .attr("cx", d => x(d.key))
-  .attr("cy", d =>y(d.avg[1].mean_budget))
-  .attr("r", d=>z(d.count[1].count))
-  // .style("fill", d => myColor(d.key))
+  .attr("class", function(d) { return "bubbles " + d.year })
+  .attr("cx", d => x(d.year))
+  .attr("cy", d =>y(d.avg_budget))
+  .attr("r", d=>z(d.count_movies))
+  .style("fill", d => myColor(d.viz_results))
+  .style("opacity", "0.7")
 
 
-// svg
-//     .append("g")
-//     .selectAll("dot")
-//     .data(root.descendants())
-//     .join("circle")
-//     .attr("cx", d => x(d.key))
-//     .attr("cy",  d => y(d.value.mean_budget))
-//     .attr("r", 6)
+  })
+
+//Read the data
+d3.csv("movies_1997_2013.csv").then(function (data) {
+  data.forEach(function (d) {
+    d["budget"] = +d["budget"];
+    d["domgross"] = +d["domgross"];
+    d["intgross"] = +d["intgross"];
+    
+    // console.log(d)
+  });
+ok = Object.keys(data[0])
+console.log(ok)
+
+
+// average budget by results + year
+avgBudget = d3.rollups(
+  data,
+  v => ({
+    count: v.length,
+    mean_budget: d3.mean(v, d => +d.budget), // get the average estimated project cost!
+  }),
+  d => d.year,
+  d => d.viz_results
+  
+)
+console.log(avgBudget)
+
+console.log(avgBudget[0][1])
+
+aB_data = avgBudget.map(d => {
+  let obj = {};
+  const [key, type, vals] = d;
+  obj = { key: key, type: d.values.keys, vals: d.values.values};
+  // obj = {key: key, type: entries[0], vals: entries(count), vals2: entries(mean_budget)}
+  return obj;
+  
+})
+console.log(aB_data)
+
    
 
 
